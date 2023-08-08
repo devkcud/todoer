eaebf2b4ef3a7f10862ad7d09b1531abf3389503
André Luis <andrescalisejr@gmail.com>
2023-08-07 23:48:37 -0300

    add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,13 @@
+<script>
+    import '$lib/styles/errorpage.scss';
+
+    import { page } from '$app/stores';
+</script>
+
+<article>
+    <section>
+        <p>Oh no!</p>
+        <h1>A wild <span id="page-status">{$page.status}</span> has appeared!</h1>
+        <p><span>At least we got it:</span> {$page.error?.message}</p>
+    </section>
+</article>
