93a92ccdf0c5ac9b7da75275d4e5fba4db3d7096
André Luis <andrescalisejr@gmail.com>
2023-08-07 21:02:25 -0300

    split styling into scss files

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
+    import '$lib/styles/__global__.scss';
+
     import GitHub from '$lib/icons/GitHub.svelte';
 </script>
 
 <header>
-    <a href="/demo">Click to see demo</a>
+    <a href="demo">Click to see demo</a>
     <a href="https://github.com/devkcud/todoer">
         <GitHub />
     </a>
@@ -12,33 +14,3 @@
 <main>
     <slot />
 </main>
-
-<style lang="scss">
-    header {
-        width: 100%;
-
-        position: absolute;
-
-        display: flex;
-        justify-content: space-between;
-
-        padding: 16px;
-
-        a {
-            color: #868686;
-            font-family: Inter;
-            font-size: 18px;
-            font-style: normal;
-            font-weight: 400;
-            line-height: normal;
-            text-decoration-line: underline;
-        }
-    }
-
-    main {
-        min-height: 100vh;
-
-        background: linear-gradient(135deg, #04060c 0%, #0f1421 100%);
-        color: #ffffff;
-    }
-</style>
--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+    import '$lib/styles/loginregister.scss';
+
     import User from '$lib/icons/User.svelte';
     import Eye from '$lib/icons/Eye.svelte';
     import EyeDeny from '$lib/icons/EyeDeny.svelte';
@@ -36,100 +38,3 @@
 
     <span>Already have an account? <a href="login">Login</a></span>
 </article>
-
-<style lang="scss">
-    article {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-
-        min-height: 100vh;
-
-        form {
-            width: 600px;
-            height: 400px;
-
-            h1 {
-                color: #ffffff;
-
-                font-size: 5rem;
-                font-style: normal;
-                font-weight: 700;
-
-                margin-bottom: 20px;
-            }
-
-            section {
-                position: relative;
-
-                width: 100%;
-
-                display: inline-flex;
-                gap: 15px;
-
-                border-bottom: 1px solid #ffffff;
-
-                padding: 14px;
-                margin-bottom: 24px;
-
-                input {
-                    background: none;
-                    color: #ffffff;
-
-                    border: none;
-                    outline: none;
-
-                    font-size: 1.1rem;
-                    width: 100%;
-                }
-
-                button {
-                    background: none;
-                    border: none;
-
-                    &:hover {
-                        cursor: pointer;
-                    }
-                }
-            }
-
-            button#continue-button {
-                display: flex;
-                justify-content: center;
-                align-items: center;
-
-                width: 320px;
-                height: 50px;
-
-                margin-top: 2rem;
-
-                margin-inline: auto;
-
-                border: none;
-                border-radius: 10px;
-
-                background: #2c8d24;
-                color: #ffffff;
-
-                font-size: 24px;
-                font-style: normal;
-                font-weight: 700;
-
-                &:hover {
-                    cursor: pointer;
-                }
-            }
-        }
-
-        span {
-            position: absolute;
-            bottom: 30px;
-
-            a {
-                color: #2c8d24;
-                font-weight: 700;
-            }
-        }
-    }
-</style>
