dda7d0b588cf8f2c3666a03746cbc1f785fe6a13
André Luis <andrescalisejr@gmail.com>
2023-08-08 01:14:53 -0300

    change layout and add group form

---
+++ src/routes/(form)/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import '$lib/styles/__global__.scss';
+
+    import GitHub from '$lib/icons/GitHub.svelte';
+</script>
+
+<header>
+    <a href="demo">Click to see demo</a>
+    <a href="https://github.com/devkcud/todoer">
+        <GitHub />
+    </a>
+</header>
+
+<main>
+    <slot />
+</main>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,6 @@
 <script lang="ts">
     import '$lib/styles/__global__.scss';
-
-    import GitHub from '$lib/icons/GitHub.svelte';
 </script>
-
-<header>
-    <a href="demo">Click to see demo</a>
-    <a href="https://github.com/devkcud/todoer">
-        <GitHub />
-    </a>
-</header>
 
 <main>
     <slot />
